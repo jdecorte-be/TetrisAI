67db01bd17600210cfdd7105e175c26f3cfdc247
John <john.decorte01@gmail.com>
2023-11-12 08:20:35 +0100

    py

--- game.py
+++ game.py
@@ -5,18 +5,41 @@ from PIL import ImageGrab
 import pywinctl as pwc
 import time
 import subprocess
-import pytesseract
+# import pytesserac
+import torch
+# from selenium import webdriver
+# from selenium.common.exceptions import TimeoutException
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.common.by import By
 
+
+# driver = webdriver.Chrome()
+# driver.get(driver.current_url)
 class Game:
     
     def __init__(self):
-        time.sleep(5)
+        # time.sleep(5)
         print("Game started")
         self.Player = player.Player("Les Hackathon")
         self.board = np.zeros((22, 12))
         self.piece_id = 0
+        self.piece = [[0,0,0],[0,0,0],[0,0,0]]
         self.score = 0
+        self.holes = 0
     
+    def reset(self):
+        self.board = np.zeros((22, 12))
+        self.piece_id = 0
+        self.score = 0
+        
+    def game_over(self):
+        
+        try:
+            alert = WebDriverWait(driver, 10).until(EC.alert_is_present())
+            return True
+        except TimeoutException:
+            return False
 
     def getPiece(self):
         piece = np.zeros((3, 3))
@@ -39,21 +62,17 @@ class Game:
         for key, value in all_piece.items():
             if (piece == value).all():
                 self.piece_id = key
-                
-        print(self.piece_id)
+                self.piece = value
         
-    def startNewGame(self):
-        while True:
-            self.board = np.zeros((22, 12))
-            screen = ImageGrab.grab()
-            screen = np.array(screen)
-            img = cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
-            self.detectBoard(img)
-            self.getPiece()
-            self.getScore(img)
-            self.getHoles()
+        # replace 2 upper line of matrix with 0
+        for i in range(12):
+            self.board[0][i] = 0
+        for i in range(12):
+            self.board[1][i] = 0
+        
             
     def detectBoard(self, img):
+        self.board = np.zeros((22, 12))
         rows, cols, _ = img.shape
         virtual_board = np.zeros((rows, cols, 3), dtype=np.uint8)
         
@@ -117,20 +136,51 @@ class Game:
         
         print(self.board)
         
+        
+
+    def check_collision(self, piece, pos):
+        future_y = pos["y"] + 1
+        for y in range(len(piece)):
+            for x in range(len(piece[y])):
+                if future_y + y > 22 - 1 or self.board[future_y + y][pos["x"] + x] and piece[y][x]:
+                    return True
+        return False
+
+
+
     def getNextState(self):
-        state = {}
-        
-        n_rot = 0
-        
-        if self.piece_id == "z" or self.piece_id == "s" or self.piece_id == "j":
-            n_rot = 2
-        elif self.piece_id == "i":
-            n_rot = 1
-        elif self.piece_id == "l":
-            n_rot = 3
-        else:
-            n_rot = 3
-        return state
+        def putOnMatrix(piece, pos):
+            board = np.array(self.board)
+            for y in range(len(piece)):
+                for x in range(len(piece[y])):
+                    if piece[y][x]:
+                        board[pos["y"] + y][pos["x"] + x] = 1
+            return board
+        def getMaxLenOfForm(form):
+            max_size = 0
+            for i in range(3):
+                count = 0
+                for j in range(3):
+                    if(form[i][j] == 1):
+                        count+=1
+                max_size = max(max_size, count)
+            return max_size
+            
+        states = {}
+        board = np.array(self.board)
+        curr_piece = np.array(self.piece)
+        for i in range(4):
+            # size of piece
+            valid_xs = 12 - getMaxLenOfForm(curr_piece)
+            for x in range(valid_xs):
+                pos = {"x": x, "y": 0}
+                while not self.check_collision(curr_piece, pos):
+                    pos["y"] += 1
+                board = putOnMatrix(curr_piece, {"x": pos["x"], "y": pos["y"]})
+                states[(x, i)] = self.getStateProp(board)
+                # print(states)
+            curr_piece = np.rot90(curr_piece, 1)
+        return states
     
     def getScore(self, img):
         img_data = pytesseract.image_to_string(img, config='--oem 3 --psm 11 -c tessedit_char_whitelist=0123456789')
@@ -140,14 +190,45 @@ class Game:
             self.score = int(img_lines[5])
         print("score: ",self.score)
         
+    def getHoles(self, board):
         
-    # def getHoles(self):
-    #     num_holes = 0
-    #     hei = 22
-    #     while hei > 0:
-    #         for i in range(12):
-    #             if self.board[hei][i] == 0:
-    #                 num_holes += 1
-                
-    #         hei -= 1
-    #     print("holes: ",num_holes)
+        num_holes = 0
+        for col in zip(*board):
+            row = 2
+            while row < 22 and col[row] == 0:
+                row += 1
+            num_holes += len([x for x in col[row + 1:] if x == 0])
+        print("holes: ",num_holes)
+        return num_holes
+
+    def getBumpAndHeight(self):
+        board = np.array(self.board)
+        mask = board != 0
+        invert_heights = np.where(mask.any(axis=0), np.argmax(mask, axis=0), 22)
+        h = 22 - invert_heights
+        total_height = np.sum(h)
+        currs = h[:-1]
+        next = h[1:]
+        diff = np.abs(currs - next)
+        bumpiness = np.sum(diff)
+        print("bumpiness: ",bumpiness)
+        print("height: ",total_height)
+        return bumpiness, total_height
+    
+    def getStateProp(self, board):
+        bump, height = self.getBumpAndHeight()
+        holes = self.getHoles(board)
+        return torch.FloatTensor([0, holes, bump, height])
+    
+    def step(self, action):
+        mov, rot = action
+        if(mov == LEFT):
+            self.Player.left()
+        elif(mov == RIGHT):
+            self.Player.right()
+        for _ in range(rot):
+            self.Player.up()
+        
+    
+        
+        
